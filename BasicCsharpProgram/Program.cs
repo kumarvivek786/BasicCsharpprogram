26d88063f7f00bbea893c63d1a5744b8f17d68a1
vivek <ikumarvivek786@gmail.com>
2022-09-24 22:21:56 +0530

    SwapNumbers

---
+++ BasicCsharpProgram/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCsharpProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp = 0;
+            int a = 0, b = 0;
+            Console.WriteLine("Enter Two Numbers: ");
+            Console.Write("a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Before Swapping a= {0} b= {1}", a, b);
+            temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine("After Swapping a= {0} b= {1}", a, b);
+            Console.ReadLine();
+        }
+    }
+}
