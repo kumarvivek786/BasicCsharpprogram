c07903d79549d1ecf3b803139de6cda2ed8bf44e
vivek <ikumarvivek786@gmail.com>
2022-09-26 09:53:02 +0530

    EvenOdd

--- BasicCsharpProgram/Program.cs
+++ BasicCsharpProgram/Program.cs
@@ -11,22 +11,22 @@ namespace BasicCsharpProgram
         static void Main(string[] args)
         {
 
-            int a = 0, b = 0, c = 0;
-            Console.WriteLine("Enter Three Numbers to Check Largest Of The Three");
-            Console.Write("First: ");
-            a = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Second: ");
-            b = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Third: ");
-            c = Convert.ToInt32(Console.ReadLine());
+            int num;
 
-            if (a > b && a > c)
-                Console.WriteLine("{0} is Largest", a);
-            else if (b > a && b > c)
-                Console.WriteLine("{0} is Largest", b);
-            else if (c > a && c > b)
-                Console.WriteLine("{0} is Largest", c);
+            Console.WriteLine("enter no. :");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Even Number");
+            }
+
+            else
+            {
+                Console.WriteLine("Odd Number");
+
+            }
             Console.ReadLine();
+
         }
     }
 }
