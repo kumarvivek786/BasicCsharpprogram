0cd0bf2cee2997ac3fb395a595d03645e4c13b5c
vivek <ikumarvivek786@gmail.com>
2022-09-26 09:45:02 +0530

    LargestOfThree

--- BasicCsharpProgram/Program.cs
+++ BasicCsharpProgram/Program.cs
@@ -10,15 +10,22 @@ namespace BasicCsharpProgram
     {
         static void Main(string[] args)
         {
-            int year;
-            Console.Write("Enter the Year :");
-            year = Convert.ToInt32(Console.ReadLine());
-            if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
-                Console.WriteLine("{0} is Leap Year", year);
-            else
-                Console.WriteLine("{0} is not a Leap Year", year);
 
+            int a = 0, b = 0, c = 0;
+            Console.WriteLine("Enter Three Numbers to Check Largest Of The Three");
+            Console.Write("First: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Second: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Third: ");
+            c = Convert.ToInt32(Console.ReadLine());
 
+            if (a > b && a > c)
+                Console.WriteLine("{0} is Largest", a);
+            else if (b > a && b > c)
+                Console.WriteLine("{0} is Largest", b);
+            else if (c > a && c > b)
+                Console.WriteLine("{0} is Largest", c);
             Console.ReadLine();
         }
     }
