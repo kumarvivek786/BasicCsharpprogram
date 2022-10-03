49a5da5a45507e2458353947a579758daf07939a
vivek <ikumarvivek786@gmail.com>
2022-10-03 15:03:42 +0530

    FlipCoin

--- BasicCsharpProgram/Program.cs
+++ BasicCsharpProgram/Program.cs
@@ -11,20 +11,31 @@ namespace BasicCsharpProgram
         static void Main(string[] args)
         {
 
-            int num;
 
-            Console.WriteLine("enter no. :");
-            num = Convert.ToInt32(Console.ReadLine());
-            if (num % 2 == 0)
+            Random random = new Random();
+
+
+            int n = 0, count1 = 0, count2 = 0;
+            double HeadPercent = 0, TailPercent = 0;
+            Console.WriteLine("Enter No. Of Coin Flip:");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
             {
-                Console.WriteLine("Even Number");
+                int r = random.Next(0, 2);
+                if (r < 0.5)
+                {
+                    count1++;
+                }
+                else
+                {
+                    count2++;
+                }
             }
-
-            else
-            {
-                Console.WriteLine("Odd Number");
-
-            }
+            Console.WriteLine("Head Count:" + count1);
+            Console.WriteLine("Tail Count:" + count2);
+            HeadPercent = (count1 * 100) / n;
+            TailPercent = (count2 * 100) / n;
+            Console.WriteLine("Percentage of Heads: {0}%  and Percentage of Tails: {1}%", HeadPercent, TailPercent);
             Console.ReadLine();
 
         }
